de351e14c955946055995c9202ec0d951abc8c15
NotteGatto <ast2605@gmail.com>
2021-12-16 00:58:56 +0600

    Second commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,8 +3,8 @@ package com.syntax;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("New Year is coming!");
+        for (int i = 0; i < 8; i++) {
+            System.out.println("New Year is coming! Part 2");
             Thread.sleep(2000);
 
         }
